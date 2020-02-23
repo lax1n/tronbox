e603d9b296057372204413d510063fe045dbcebc
lax1n <lax1n@users.noreply.github.com>
2020-02-23 23:08:04 +0100

    Use npm instead of yarn

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@ rm -rf build
 mkdir build
 mkdir build/components
 mkdir build/components/ContractSchema
+node_modules/.bin/babel --minified --no-comments src -d build
 cp -r src/components/ContractSchema/spec/ build/components/ContractSchema/spec/
 cp scripts/tronbox.js build/loanbox.js
 chmod +x build/loanbox.js
